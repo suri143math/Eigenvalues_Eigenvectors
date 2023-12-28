afa9eb20afb8df27c2159b90f7b503531b503303
suri143math <155049975+suri143math@users.noreply.github.com>
2023-12-28 16:10:28 +0530

    Create solve_system_equations.py

---
+++ solve_system_equations.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy.linalg import solve
+
+def solve_system():
+    A=np.array([[1,2,3],[4,3,6],[2,1,4]])
+    b=np.array([4,5,6])
+    x = solve(A, b)
+    print(x)
+
+
+solve_system()
